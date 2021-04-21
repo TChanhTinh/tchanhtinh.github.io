fb9801de306e41c2bf9d04412c3ebc48685a9197
tchanhtinh@gmail.com <tchanhtinh@gmail.com>
2021-04-21 22:42:17 +0700

    remove iframe mess, flex-wrap the about me section

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -16,19 +15,19 @@ export default function Home() {
                 Home
               </button>
               <a href="#about">
-              <button className="hover:bg-red-200 rounded p-2 cursor-pointer active:bg-red-300 focus:outline-none">
-                About Me
-              </button>
+                <button className="hover:bg-red-200 rounded p-2 cursor-pointer active:bg-red-300 focus:outline-none">
+                  About Me
+                </button>
               </a>
               <a href="#skills">
-              <button className="hover:bg-red-200 rounded p-2 cursor-pointer active:bg-red-300 focus:outline-none">
-                Skill
-              </button>
+                <button className="hover:bg-red-200 rounded p-2 cursor-pointer active:bg-red-300 focus:outline-none">
+                  Skill
+                </button>
               </a>
               <a href="#projects">
-              <button className="hover:bg-red-200 rounded p-2 cursor-pointer active:bg-red-300 focus:outline-none">
-                Projects
-              </button>
+                <button className="hover:bg-red-200 rounded p-2 cursor-pointer active:bg-red-300 focus:outline-none">
+                  Projects
+                </button>
               </a>
             </div>
           </nav>
@@ -67,8 +66,8 @@ export default function Home() {
           </div>
         </header>
         <div className="container mx-auto">
-          <div className="flex m-24 p-12 bg-white shadow ring-2 ring-red-100">
-            <div>
+          <div className="flex flex-wrap m-24 p-12 bg-white shadow ring-2 ring-red-100">
+            <div className="">
               <span className="font-bold text-xl">About me</span>
               <p>
                 My name is To Chanh Tinh, i'm a software engineering student in
@@ -89,24 +88,28 @@ export default function Home() {
                 problem. Looking forward to work with you.
               </p>
             </div>
-            <div className="ml-20">
+            <div className="mt-5">
               <span className="font-bold text-xl">Contact information</span>
-              <div className="flex p-2 space-x-24">
+              <div className="flex mt-2">
                 <span className="font-bold text-md">Email:</span>
+                <div className="w-3"/>
                 <p className="text-md">tchanhtinh@gmail.com</p>
               </div>
-              <div className="flex p-2 space-x-24">
+              <div className="flex mt-2 ">
                 <span className="font-bold text-md">Phone:</span>
+                <div className="w-3"/>
                 <p className="text-md">(+84) 918 011 823</p>
               </div>
-              <div className="flex p-2 space-x-24">
+              <div className="flex mt-2 ">
                 <span className="font-bold text-md">Address:</span>
+                <div className="w-3"/>
                 <p className="text-md">
                   Bến Vân Đồn, Phường 14, Quận 6, TP. Hồ Chí Minh
                 </p>
               </div>
-              <div id="skills" className="flex p-2 space-x-24">
+              <div id="skills" className="flex mt-2">
                 <span className="font-bold text-md">Language:</span>
+                <div className="w-3"/>
                 <p className="text-md">Vietnamese, English</p>
               </div>
             </div>
@@ -119,11 +122,11 @@ export default function Home() {
               <p className="font-bold">On web development</p>
               <ul>
                 <li>
-                  - I'm most famillar with web development using MERN Stack.
+                  - I'm most famillar with web development using NodeJS,
+                  ReactJS, PostgreSQL stack.
                 </li>
                 <li>
-                  - I know how to make REST API application and microservices
-                  application.
+                  - I know how to make REST API and microservices application.
                 </li>
               </ul>
               <p className="font-bold">DevOps relate skills</p>
@@ -169,8 +172,8 @@ export default function Home() {
               <p className="font-bold">Learning</p>
               <ul>
                 <li>
-                  - I'm learing Elixir to bring the power of Erlang VM to make
-                  concurrency based application.
+                  - I'm learing Elixir to bring the power of Erlang VM for
+                  making scalable concurrency based application.
                 </li>
                 <li id="projects">
                   - I'm looking to learn automation tools like Chef, Kubertenes,
@@ -184,95 +187,76 @@ export default function Home() {
             My projects
           </span>
           <div className="flex m-24 mt-8 p-12 bg-white shadow ring-2 ring-red-100">
-            <div>
+            <div className="">
               <p className="font-bold">Timeline</p>
               <ul className="flex flex-col space-y-2">
                 <li>
-                  - Jun 14 2019: Canopus-Showcase, work with a team
-                  in FSoft as a intern on a project that visualization OCR JSON data to web
+                  - Jun 14 2019: Canopus-Showcase, work with a team in FSoft as
+                  a intern on a project that visualization OCR JSON data to web
                   application.
                 </li>
-                <a target="_blank" className="text-blue-600" href="https://github.com/tchanhtinh/catpaws">
-                <li>
-                  - Jul 14 2020: Catpaws, a simple demo how proof of work
-                  technique in blockchain work. Technology used: NodeJS as
-                  back-end, React for front-end, PostgreSQL for database and Go
-                  as REST API service for mining.
-                </li>
+                <a
+                  target="_blank"
+                  className="text-blue-600"
+                  href="https://github.com/tchanhtinh/catpaws"
+                >
+                  <li>
+                    - Jul 14 2020: Catpaws, a simple demo how proof of work
+                    technique in blockchain work. Technology used: NodeJS as
+                    back-end, React for front-end, PostgreSQL for database and
+                    Go as REST API service for mining.
+                  </li>
                 </a>
-                <a target="_blank" className="text-blue-600" href="https://github.com/tchanhtinh/robot">
-                <li>
-                  - Sep 21 2020: Veternary Husbandy, a web and mobile
-                  application dictionary for veter. Technology used: NodeJS as
-                  back-end, React for front-end, PostgreSQL for database and
-                  React Native, SqLite for mobile platform.
-                </li>
+                <a
+                  target="_blank"
+                  className="text-blue-600"
+                  href="https://github.com/tchanhtinh/robot"
+                >
+                  <li>
+                    - Sep 21 2020: Veternary Husbandy, a web and mobile
+                    application dictionary for veter. Technology used: NodeJS as
+                    back-end, React for front-end, PostgreSQL for database and
+                    React Native, SqLite for mobile platform.
+                  </li>
                 </a>
-                <a target="_blank" className="text-blue-600" href="http://se.cit.ctu.edu.vn">
                 <li>
-                  - Jan 27 2021 (Maintaining): Secit, working with a team to
-                  build website for a University Department. Technology used:
-                  NodeJS as back-end, VueJS for front-end, PostgreSQL for
-                  database.
+                  <a
+                    target="_blank"
+                    className="text-blue-600"
+                    href="http://se.cit.ctu.edu.vn"
+                  >
+                    - Jan 27 2021 (Maintaining): Secit, working with a team to
+                    build website for a University Department. Technology used:
+                    NodeJS as back-end, VueJS for front-end, PostgreSQL for
+                    database.
+                  </a>
                 </li>
-                </a>
-                <a target="_blank" className="text-blue-600" href="https://github.com/tchanhtinh/dotfiles-that-suckless">
                 <li>
-                  - Feb 19 2021 (Maintaining): Dotfiles-that-suckless, a
-                  personal dotfiles and scripts setup with customized version
-                  DWM window manager for Arch Linux.
+                  <a
+                    target="_blank"
+                    className="text-blue-600"
+                    href="https://github.com/tchanhtinh/dotfiles-that-suckless"
+                  >
+                    - Feb 19 2021 (Maintaining): Dotfiles-that-suckless, a
+                    personal dotfiles and scripts setup with customized version
+                    DWM window manager for Arch Linux.
+                  </a>
                 </li>
-                </a>
-                <a target="_blank" className="text-blue-600" href="https://github.com/pystardust/ytfzf">
                 <li>
-                  - Mar 24 2021 (Contribute): ytfzf, work as a contributor for
-                  ytfzf project, POSIX script that helps you find Youtube video
-                  on CLI.
+                  <a
+                    target="_blank"
+                    className="text-blue-600"
+                    href="https://github.com/pystardust/ytfzf"
+                  >
+                    - Mar 24 2021 (Contribute): ytfzf, work as a contributor for
+                    ytfzf project, POSIX script that helps you find Youtube
+                    video on CLI.
+                  </a>
                 </li>
-                </a>
               </ul>
-              <div>
-                <p className="font-bold mt-4">Live2D Projects</p>
-                <div className="flex space-x-8 m-5">
-                  <div className="flex flex-col space-y-8">
-                    <div className="flex space-x-14">
-                      <iframe
-                        id="ytplayer"
-                        src="https://www.youtube.com/embed/4yORgcxv1Ec"
-                        width="580px"
-                        height="360px"
-                        frameBorder="0"
-                      />
-                      <iframe
-                        id="ytplayer"
-                        src="https://www.youtube.com/embed/Wtlh6792kNg"
-                        width="580px"
-                        height="360px"
-                        frameBorder="0"
-                      />
-                    </div>
-                    <div className="flex space-x-14">
-                      <iframe
-                        id="ytplayer"
-                        src="https://www.youtube.com/embed/o4lKrkIdEDI"
-                        width="580px"
-                        height="360px"
-                        frameBorder="0"
-                      />
-                      <iframe
-                        id="ytplayer"
-                        src="https://www.youtube.com/embed/bshu4--YQMU"
-                        width="580px"
-                        height="360px"
-                        frameBorder="0"
-                      />
-                    </div>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
-        </div>
         <footer>
           <div className="flex flex-col justify-center text-center text-gray-600 font-serif p-4">
             <p>Made by: TChanhTinh</p>
